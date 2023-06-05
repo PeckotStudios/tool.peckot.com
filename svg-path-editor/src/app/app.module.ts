942a2aaa33cb183a15f86dc8c5b3105ab195b2c6
Pectics <pectics@outlook.com>
2023-06-06 07:16:45 +0800

    30000->5000

--- svg-path-editor/src/app/app.module.ts
+++ svg-path-editor/src/app/app.module.ts
@@ -78,7 +78,7 @@ import { ShareComponent, ShareDialogComponent, ShareDialogSnackbarComponent } fr
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:5000'
     })
   ],
   providers: [{
